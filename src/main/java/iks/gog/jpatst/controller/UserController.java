af38f4a71d9d1c98e98c9e56f053bf01d24b6579
iks <iks.gog@gmail.com>
2017-08-09 23:36:34 +0300

    add security

--- src/main/java/iks/gog/jpatst/controller/UserController.java
+++ src/main/java/iks/gog/jpatst/controller/UserController.java
@@ -59,13 +59,4 @@ public class UserController {
         return "login";
     }
 
-    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
-        return "welcome";
-    }
-
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String admin(Model model) {
-        return "admin";
-    }
 }
